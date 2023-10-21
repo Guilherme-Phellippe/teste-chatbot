7c24465d1e8fdc3e4eabd3e12433232e34cfd10f
guilherme <guilhermephellippe@outlook.com>
2023-10-21 14:16:44 -0300

    create the first version interface whatsapp

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,25 +1,75 @@
+import { Camera, DotsThreeVertical, Keyboard, Microphone, PaperPlaneRight, Paperclip, Phone, VideoCamera } from "@phosphor-icons/react";
+
 function Chat() {
     return (
-        <div className="w-screen h-screen grid place-items-center">
-            <div className="w-full h-[95%] max-w-[1600px] rounded-sm bg-blue_medium_dark flex">
-                <div className="w-[30%] border-r-[1px] border-zinc-300/30">
+        <div className="w-screen h-screen flex justify-center items-center">
+            <div className="w-full h-full md:h-[95%] max-w-[480px] rounded-sm bg-blue_medium_dark flex">
 
-                </div>
-                <div className="w-[70%] flex flex-col justify-between">
-                    <header className="w-full h-[59px] bg-blue_light_dark">
-                        nome do usuário
+                <div className="w-full h-full flex flex-col justify-between">
+
+
+                    <header className="w-full h-[70px] bg-green_main flex justify-around items-center">
+
+                        <div className="w-[15%] flex justify-center">
+                            <div className="w-[40px] h-[40px] rounded-full">
+                                <img
+                                    src="https://i.ibb.co/XFHL0Bz/1000-F-589932782-v-QAEAZh-Hnq1-QCGu5ikwr-Ya-QD0-Mmurm0-N-removebg-preview.png"
+                                    alt="imagem do avatar"
+                                    className="w-full h-full object-contain"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="w-[50%] cursor-pointer">
+                            <p>Nome do contato</p>
+                        </div>
+
+                        <div className="w-[40%] flex justify-evenly items-center">
+                            <VideoCamera
+                                size={32}
+                                className="cursor-pointer"
+                            />
+                            <Phone size={32} className="cursor-pointer" />
+                            <DotsThreeVertical size={32} className="cursor-pointer" />
+                        </div>
+
                     </header>
-                    <main className="w-full h-full bg-[url(https://i.ibb.co/SVh0S75/Design-sem-nome-removebg-preview.png)] opacity-10">
 
+
+
+                    <main className="w-full h-full bg-[url(https://i.ibb.co/SVh0S75/Design-sem-nome-removebg-preview.png)] opacity-10">
+                         
                     </main>
-                    <footer className="w-full min-h-[62px] bg-[#202c33] flex justify-around items-center">
-                        <input 
-                            type="text"
-                            className="w-4/5 p-2 rounded-lg bg-[#2a3942] outline-none"
-                            placeholder="Digite sua mensagem"
-                        />
+
+
+                    <footer className="w-full h-[65px] mb-4 flex items-center">
+                        <div className="w-4/5 mx-4 p-2 rounded-3xl flex gap-2 items-center bg-white">
+                            <Keyboard
+                                size={32}
+                                className="fill-zinc-600/70"
+                            />
+                            <input
+                                type="text"
+                                className="w-[70%] bg-white text-zinc-800 outline-none"
+                                placeholder="Messagem"
+                            />
+                            <Paperclip
+                                size={32}
+                                className="fill-zinc-600/70"
+                            />
+                            <Camera
+                                size={32}
+                                className="fill-zinc-600/70"
+                            />
+                        </div>
+
+                        <div className="w-[50px] bg-green_light mr-2 rounded-full grid place-items-center p-2 cursor-pointer">
+                            {/* <Microphone size={32} /> */}
+                            <PaperPlaneRight size={28} />
+                        </div>
                     </footer>
                 </div>
+
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ export default {
         blue_dark: "#0c1317",
         blue_medium_dark: "#111b21",
         blue_light_dark: "#2a3942",
+        green_main: "#008069",
+        green_light: "#00a884"
       }
     },
   },
