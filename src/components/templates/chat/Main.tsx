2fb812ddfed78692097ef9d7fdadce7c26a61d07
guilherme <guilhermephellippe@outlook.com>
2023-10-21 15:44:40 -0300

    create simple chat dynamic

--- src/components/templates/chat/Main.tsx
+++ src/components/templates/chat/Main.tsx
@@ -26,8 +26,8 @@ function Main() {
                 messages &&
                 messages.map((msg, index) =>
                     <div key={index} className={`w-auto flex ${msg.player === 1 ? "justify-end" : "justify-start"} items-center`}>
-                        <div className="px-4 py-1 max-w-[70%] rounded-2xl bg-blue_light_dark flex gap-2">
-                            {msg.message}
+                        <div className="px-4 py-1 max-w-[70%] rounded-2xl bg-blue_light_dark flex flex-col">
+                            <p className="">{msg.message}</p>
                             <span className="text-zinc-300/50 text-sm self-end">{msg.time}</span>
                         </div>
                     </div>
